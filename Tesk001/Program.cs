f1bb3d01dbe44e26159407189b819d178af5d580
Marina <marra@inbox.ru>
2022-11-24 18:54:37 +0300

    HomeWork3

---
+++ Tesk001/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+int GetInvertedNumbers(int number)
+{
+    int ReverseNumber = 0;
+    int result = 0;
+    int DigitNumber = 10;
+    while (number != 0)
+    {
+        ReverseNumber = number % 10;
+        result = result * DigitNumber + ReverseNumber;
+        number = number / 10;
+    }
+    return result;
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    Console.WriteLine($"Число {number} не является палиндромом");
+}
+else
+{
+    int ReverseNumber = GetInvertedNumbers(number);
+    if (number == ReverseNumber)
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}
+
---
+++ Tesk002/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+int GetCoordinateFromUser(string l, string c)
+{
+    Console.Write($"Введите координату {l} точки {c}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double GetDistanceIn3D(int Ax, int Ay, int Az, int Bx, int By, int Bz)
+{
+    return Math.Abs(Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Bz - Az, 2)));
+}
+
+int Ax = GetCoordinateFromUser("х", "А");
+int Ay = GetCoordinateFromUser("y", "А");
+int Az = GetCoordinateFromUser("z", "А");
+int Bx = GetCoordinateFromUser("х", "B");
+int By = GetCoordinateFromUser("y", "B");
+int Bz = GetCoordinateFromUser("z", "B");
+double distance = GetDistanceIn3D (Ax, Ay, Az, Bx, By, Bz);
+Console.WriteLine($"Растояние между точками: {distance}");
+
